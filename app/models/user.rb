86d01cd5e0db24c5616f2a2226144f90f41c0416
Carri-anne Hamer <chamer79@gmail.com>
2022-03-30 15:52:46 -0400

    rails db:migrate. in user model, added error messages & uniquness to email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
  
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
+  validates :first_name, presence: { message: "*First name is required." }
+  validates :last_name, presence: { message: "*Last name is required." }
+  validates :email, presence: { message: "*Email is required." }, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :city, presence: true
-  validates :state, presence: true
-  validates :score, presence: true
+  validates :city, presence: { message: "*City is required." }
+  validates :state, presence: { message: "*State is required." }
+  validates :score, numericality: { only_integer: true }
 
 end
